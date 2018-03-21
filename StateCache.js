536e4b8dac5d0b59baa470963021db331348c03f
azoth <zhou.vip@icloud.com>
2018-03-21 14:22:40 +0800

    Update StateCache.js

--- StateCache.js
+++ StateCache.js
@@ -1,48 +1,66 @@
-import React from 'react';
 /*
   config.key 必须 作为sessionstorage的key值
+  maxNum 最大缓存条数
+  storageType 缓存类型
+  lifecycle 缓存的生命周期
  */
-const StateCache = (config) => {
+const stateCache = (config) => {
   console.log('stateCache');
   return (Comp) => {
     const defaultConfig = {
       key: '',
       maxNum: 15,
+      storageType: 'sessionStorage',
+      lifecycle: ['componentWillUnmount'],
     };
     const warpConfig = { ...defaultConfig, ...config };
     class WrapComp extends Comp {
       componentWillMount() {
-        console.log('willmount')
-        let preState = {};
-        const hashQueryName = `$${this.getQuery()[warpConfig.hashQueryName]}`;
-        const storageDataStr = sessionStorage.getItem(warpConfig.key);
-
-        if (storageDataStr) {
-          console.log('useCache');
-          preState = JSON.parse(storageDataStr)[hashQueryName];
-        }
-        console.log('componentWillmount', preState);
-        if (preState) {
-          this.setState(preState);
-        }
+        console.log('componentWillmount', this.getStorageDataItem());
+        this.setState(this.getStorageDataItem());
         super.componentWillMount();
       }
 
+      componentDidUpdate() {
+        if (warpConfig.lifecycle.includes('componentDidUpdate')) {
+          this.storeState();
+        }
+        super.componentDidUpdate();
+      }
+
       componentWillUnmount() {
-        // todo:cache state in sessionstorage
-        const hashQueryName = `$${this.getQuery()[warpConfig.hashQueryName]}`;
-        const storageDataStr = sessionStorage.getItem(warpConfig.key);
-        let storageData = {};
-        if (storageDataStr) {
-          storageData = JSON.parse(storageDataStr);
+        if (warpConfig.lifecycle.includes('componentWillUnmount')) {
+          this.storeState();
         }
+        super.componentWillUnmount();
+      }
+      storeState() {
+        const hashQueryName = this.getQueryName();
+        const storageData = this.getStorageData();
         storageData[hashQueryName] = this.state;
-        const storageKeys = Object.keys(storageData);
         while (Object.keys(storageData).length > warpConfig.maxNum) {
-          delete storageData[storageKeys[0]];
+          delete storageData[Object.keys(storageData)[0]];
         }
-        console.log('componentWillUnmount', JSON.stringify(storageData));
-        sessionStorage.setItem(warpConfig.key, JSON.stringify(storageData));
+        window[warpConfig.storageType].setItem(warpConfig.key, JSON.stringify(storageData));
+      }
+
+      // 获取整key对应的整体storage
+      getStorageData() {
+        const storageDataStr = window[warpConfig.storageType].getItem(warpConfig.key);
+        return (storageDataStr && JSON.parse(storageDataStr)) ? JSON.parse(storageDataStr) : {};
+      }
+      // 获取key的storage中，hashname对应的item
+      getStorageDataItem() {
+        const hashQueryName = this.getQueryName();
+        return this.getStorageData()[hashQueryName] || {};
+      }
+      // 获取hash的key值
+      getQueryName() {
+        let res = '$a';
+        if (warpConfig.hashQueryName && this.getQuery()[warpConfig.hashQueryName]) {
+          res = `$${this.getQuery()[warpConfig.hashQueryName]}`;
+        }
+        return res;
       }
     }
     if (!config.key) {
@@ -53,4 +71,4 @@ const StateCache = (config) => {
   };
 };
 
-export default StateCache;
+export default stateCache;
